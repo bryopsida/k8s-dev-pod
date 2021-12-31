05fa1d47409aa1b87b71cc5648725f7c9c6080b9
bryopsida <8363252+bryopsida@users.noreply.github.com>
2021-12-31 09:14:16 -0600

    add exist check before making directory

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -5,7 +5,7 @@ echo "Your password is: $PASSWORD"
 echo -e "$PASSWORD\n$PASSWORD" | sudo passwd developer
 
 # setup permissions
-sudo mkdir /etc/dropbear
+[ -d /etc/dropbear ] || sudo mkdir /etc/dropbear
 sudo chmod 700 /etc/dropbear
 sudo chown -R developer:developer /etc/dropbear
 touch /etc/dropbear/authorized_keys
