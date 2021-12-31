c0149c003bedc470fae95f7fe84afe9ccd22b817
bryopsida <8363252+bryopsida@users.noreply.github.com>
2021-12-31 09:15:07 -0600

    add -p to mkdir

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -5,7 +5,7 @@ echo "Your password is: $PASSWORD"
 echo -e "$PASSWORD\n$PASSWORD" | sudo passwd developer
 
 # setup permissions
-[ -d /etc/dropbear ] || sudo mkdir /etc/dropbear
+sudo mkdir -p /etc/dropbear
 sudo chmod 700 /etc/dropbear
 sudo chown -R developer:developer /etc/dropbear
 touch /etc/dropbear/authorized_keys
