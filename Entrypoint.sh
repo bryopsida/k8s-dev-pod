3509c303edad4b612302abdb1b0b7e6e49a4489b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-01-02 20:07:32 +0000

    add option to disable password login

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
-# Create a random password for developer and echo it to the console
-PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo "Your password is: $PASSWORD"
-echo "developer:$PASSWORD"| sudo chpasswd
+#!/bin/bash
 
 # setup permissions
 sudo mkdir -p /etc/dropbear
@@ -16,4 +12,15 @@ sudo chown -R developer:developer /home/developer
 # this occurs on every container startup and is meant to be idempotent without overwriting existing files
 /usr/local/bin/prepare_home.sh
 
-exec dropbear -R -w -F -E -p 3022 -P /var/run/dropbear.pid
+passwordLoginEnabled=${PASSWORD_LOGIN_ENABLED:-"false"}
+if [[ $passwordLoginEnabled == "true" ]]; then
+  # Create a random password for developer and echo it to the console
+  PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo "developer:$PASSWORD"| sudo chpasswd
+  echo "Your password is: $PASSWORD"
+  exec dropbear -R -w -F -E -p 3022 -P /var/run/dropbear.pid
+else
+  echo "Password login is disabled"
+  sudo passwd -d developer
+  exec dropbear -R -w -F -E -s -p 3022 -P /var/run/dropbear.pid
+fi
